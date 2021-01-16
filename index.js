957e703010609f5a0ccbd4b55853fddbacc904ca
imsachinss25 <63443790+imsachinss25@users.noreply.github.com>
2021-01-16 21:30:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index2.css';
+import App from './App';
+import App2 from './keep/App2';
+
+
+
+ReactDom.render(
+<>
+<App2 />
+</>,
+document.getElementById('root')	
+);
